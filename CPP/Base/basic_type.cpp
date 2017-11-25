8d4cbd45b2cfaeb0956dd5064c7c663165e1d7f0
KuoPing Kao <igaozp@hotmail.com>
2017-11-25 10:09:17 +0800

    [Add] 添加基本数据类型 basic_type.cpp

---
+++ CPP/Base/basic_type.cpp
@@ -0,0 +1,28 @@
+//
+// Created by igaozp on 2017/11/25.
+//
+
+#include <iostream>
+
+/**
+ * 基本数据类型
+ *
+ * @return 0
+ */
+int main() {
+    int integer = 0;
+    long long_number = 0;
+    bool boolean = true;
+    char character = 'A';
+    float float_number = 0.0;
+    double double_number = 0.0;
+
+    std::cout << integer << std::endl;
+    std::cout << long_number << std::endl;
+    std::cout << boolean << std::endl;
+    std::cout << character << std::endl;
+    std::cout << float_number << std::endl;
+    std::cout << double_number << std::endl;
+
+    return 0;
+}
