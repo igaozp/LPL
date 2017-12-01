1b21817b5e921f40a9a7d285a8858cfcff189eca
KuoPing Kao <igaozp@hotmail.com>
2017-12-01 19:45:44 +0800

    [Add] 添加简单的函数使用方法 function.cpp

---
+++ CPP/Base/function.cpp
@@ -0,0 +1,15 @@
+//
+// Created by igaozp on 2017/12/1.
+//
+
+#include <iostream>
+
+void print() {
+    std::cout << "Hello Function" << std::endl;
+}
+
+int main() {
+    print();
+
+    return 0;
+}
