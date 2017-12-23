02369b6d8ee8192d6c4211dc18902e400e944aa4
KuoPing Kao <igaozp@hotmail.com>
2017-12-23 14:36:54 +0800

    [Add] Nullable types

---
+++ Kotlin Koans/lesson1/task8/src/Task.kt
@@ -0,0 +1,13 @@
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+    if (message != null && client != null && client.personalInfo != null && client.personalInfo.email != null) {
+        mailer.sendMessage(client.personalInfo.email, message)
+    }
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
