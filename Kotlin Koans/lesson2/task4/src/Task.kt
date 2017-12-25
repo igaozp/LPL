fd79b6b0ea32c9d3a132245a6f080f659ffa4164
KuoPing Kao <igaozp@hotmail.com>
2017-12-25 18:08:38 +0800

    [Add] For loop

---
+++ Kotlin Koans/lesson2/task4/src/Task.kt
@@ -0,0 +1,23 @@
+class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(start, end)
+}
+
+class DateIterator(val start: MyDate, val end: MyDate) : Iterator<MyDate> {
+    var hasNext = start <= end
+    var next = if (hasNext) start else end
+    
+    override fun hasNext(): Boolean = hasNext
+
+    override fun next(): MyDate {
+        val result = next
+        next = next.nextDay()
+        hasNext = next <= end
+        return result
+    }
+}
+
+fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
+    for (date in firstDate..secondDate) {
+        handler(date)
+    }
+}
