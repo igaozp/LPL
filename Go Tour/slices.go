430485c7b255757bd56efb0e72f28b0bd65ea4fb
Fruent Neon <igaozp@hotmail.com>
2018-01-14 19:05:10 +0800

    [Add] slices.go

---
+++ Go Tour/slices.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 元素类型为 int 的 slice
+	s := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("s ==", s)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("s[%d] == %d\n", i, s[i])
+	}
+}
