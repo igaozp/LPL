9025f3b9dbc682ae353f04860d27548858960e75
Fruent Neon <igaozp@hotmail.com>
2018-01-15 17:41:49 +0800

    [Add] map literals

--- Go Tour/map.go
+++ Go Tour/map.go
@@ -8,10 +8,21 @@ type Mapper struct {
 
 var m map[string]Mapper
 
+var n = map[string]Mapper{
+	"Google": Mapper{
+		37.4333, -122.4356,
+	},
+	"Facebook": {
+		57.4245, -52.3242,
+	},
+}
+
 func main() {
 	m = make(map[string]Mapper)
 	m["Bell Labs"] = Mapper{
 		34.2345, -23.5467,
 	}
+
 	fmt.Println(m["Bell Labs"])
+	fmt.Println(n)
 }
