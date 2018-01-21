a8b8c58fe749d96b191e3dd752e5f11ec970db52
Fruent Neon <igaozp@hotmail.com>
2018-01-21 19:20:26 +0800

    [Add] default-selection.go

---
+++ Go Tour/default-selection.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		// 当 select 中的其他条件分支都没有准备好的时候，
+		// default 分支会被执行
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
